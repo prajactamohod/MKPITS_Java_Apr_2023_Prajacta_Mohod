8dc930b140dd07d29fce0967421bad7eb5f38d11
unknown <prajumohod@gmail.com>
2023-06-16 07:59:43 +0530

    inheritance

---
+++ Java/computerInheritance.java
@@ -0,0 +1,30 @@
+class Computers{
+string keyboard;
+string mouse;
+string screen;
+string Processor;
+String cpu;
+}
+class PC{
+string keyboard;
+string screen;
+string mouse;
+int size;
+}
+class Laptop{
+int size;
+string touchpad;
+string camera;
+string fingerprint;
+}
+employee laptop extends laptop{
+string coding;
+string designing;
+string documents;
+String Processor;
+}
+gameing laptop extends laptop{
+string gameing;
+string editing;
+string Processor;
+}
