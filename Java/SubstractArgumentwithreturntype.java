8fc2d2aa7cc3cc4027439f8dd9163ad77db496fc
unknown <prajumohod@gmail.com>
2023-06-19 18:41:59 +0530

    substractArgumentwithereturntype

---
+++ Java/SubstractArgumentwithreturntype.java
@@ -0,0 +1,11 @@
+class SubstractArgumentwithreturntype{
+//Argumentwithreturntype-------------
+public int Substraction(int number1,int number2){
+return number1+number2;
+}
+public static void main(String[]args){
+SubstractArgumentwithreturntype cal=new SubstractArgumentwithreturntype();
+int sub=cal.Substraction(40,22);
+System.out.println("sum of two number="+sub);
+}
+}
