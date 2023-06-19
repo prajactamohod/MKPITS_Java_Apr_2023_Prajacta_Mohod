2f0b454b5968e17de48db5c9c3d70a04db41a73a
unknown <prajumohod@gmail.com>
2023-06-19 11:48:43 +0530

    java example

---
+++ Java/NoargumentNoreturnaddition.java
@@ -0,0 +1,11 @@
+class  NoargumentNoreturnaddition{
+public void NoargumentNoreturnaddition(){
+int number1=20,number2=10;
+int add=number1+number2;
+System.out.println("addition of 2-number="+add);
+}
+public static void main(String[]args){
+NoargumentNoreturnaddition add=new NoargumentNoreturnaddition();
+add.NoargumentNoreturnaddition();
+}
+}
---
+++ Java/NoargumentNoreturnmultiplication.java
@@ -0,0 +1,11 @@
+class NoargumentNoreturnmultiplication{
+public void NoargumentNoreturnmultiplication(){
+int number1=3,number2=5;
+int multiply=number1*number2;
+System.out.println("multiply of 2-number="+multiply);
+}
+public static void main(String[]args){
+NoargumentNoreturnmultiplication multiply=new NoargumentNoreturnmultiplication();
+multiply.NoargumentNoreturnmultiplication();
+}
+}
