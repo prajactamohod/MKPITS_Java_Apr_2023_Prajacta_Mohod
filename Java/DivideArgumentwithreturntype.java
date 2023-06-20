bb53e0a6eb37c657cb9e57a848c6b6aa8cb4b27c
unknown <prajumohod@gmail.com>
2023-06-20 10:46:36 +0530

    divideargumnetwithreturntype

---
+++ Java/DivideArgumentwithreturntype.java
@@ -0,0 +1,11 @@
+class DivideArgumentwithreturntype{
+//Argumentwithreturntype-------------
+public int Divide (int number1,int number2){
+return number1+number2;
+}
+public static void main(String[]args){
+DivideArgumentwithreturntype cal=new DivideArgumentwithreturntype();
+int div=cal.Divide(80,2);
+System.out.println("sum of two number="+div);
+}
+}
