d0206d426526015e7ba05ae4a5d91e59c0a9e83b
unknown <prajumohod@gmail.com>
2023-08-09 16:23:32 +0530

    PraticeUnicodeProgram

---
+++ Java/UnicodeProgram/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String s1 = "Nagpur";
+        float marks=90f;
+        int s2=907;
+        System.out.println(String.format("My City: %s",s1));
+        System.out.println(String.format("My Marks:%f",marks));
+        System.out.println(String.format("Hex:%x octal:%o",s2,s2));
+    }
+}
