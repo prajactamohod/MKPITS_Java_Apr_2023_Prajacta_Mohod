046083964e49b0cebaae9f326b5c54a95e556ed1
unknown <prajumohod@gmail.com>
2023-08-20 16:06:31 +0530

    DataTypeProgram

---
+++ Java/Integerdatatype.java
@@ -0,0 +1,10 @@
+class Integerdatatype {
+public int method(int number1){
+return number1;
+}
+public static void main(String[]args){
+Integerdatatype data=new Integerdatatype();
+int range=data.method(9994080834);
+System.out.println("integer number is valid"+range);
+}
+}
