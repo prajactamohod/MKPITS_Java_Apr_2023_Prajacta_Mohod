518635cfb1e940c993a524b86423f982f23b92e9
unknown <prajumohod@gmail.com>
2023-08-20 16:38:53 +0530

    ProgramNoArgumentReturntype

---
+++ Java/NoargumentNoreturnsubstration.java
@@ -0,0 +1,11 @@
+class  NoargumentNoreturnsubstration{
+public void NoargumentNoreturnsubstration(){
+int number1=20,number2=10;
+int substract=number1-number2;
+System.out.println("substract of 2-number="+substract);
+}
+public static void main(String[]args){
+NoargumentNoreturnsubstration substract=new NoargumentNoreturnsubstration();
+substract.NoargumentNoreturnsubstration();
+}
+}
