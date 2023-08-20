f3df6495f2ef37d11c3d2a739d07536fa45d799e
unknown <prajumohod@gmail.com>
2023-08-20 16:16:34 +0530

    SumArgumentWithreturnTypeProgram

---
+++ Java/SumArgumentwithreturntype.java
@@ -0,0 +1,11 @@
+class SumArgumentwithreturntype{
+//Argumentwithreturntype-------------
+public int addition(int number1,int number2){
+return number1+number2;
+}
+public static void main(String[]args){
+SumArgumentwithreturntype cal=new SumArgumentwithreturntype();
+int add=cal.addition(12,22);
+System.out.println("sum of two number="+add);
+}
+}
