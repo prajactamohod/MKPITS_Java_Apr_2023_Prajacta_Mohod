e20a851c5d9c5d0b1b7456c44231a9901c76cefe
unknown <prajumohod@gmail.com>
2023-09-27 23:45:53 +0530

    Update_Query

---
+++ SQL/College_Festival_Event_DataBase/Update_Query.sql
@@ -0,0 +1,71 @@
+create database CollegeFestival;
+use CollegeFestival;
+create table Student
+(
+stud_id char(6) primary key,
+stud_name char(6),
+city_name char(15),
+address varchar(25),
+mobileno varchar(10)
+)
+
+select *from Student
+(
+stud_id char(6) primary key,
+stud_name char(6),
+city_name char(15),
+address varchar(25),
+mobileno varchar(10)
+)
+
+select *from student
+insert into student values('1001','Prince','Amravati','Professor colony','8202662347') 
+insert into student values('1003','Paul','Amravati','Professor colony','0987654321') 
+insert into student values('1004','Riya','Chennai','Forest colony','1234567890') 
+
+create table Festival
+(
+fest_id char(10),
+fest_name char(10),
+fest_participant_stud varchar(25),
+stud_id char(6),
+constraint college_fest foreign key (stud_id) references Student (stud_id) 
+)
+
+select * from Festival
+select * from Student
+insert into Festival values('2023_1','Robotics','Prince','1001') 
+insert into Festival values('2023_2','Workshop','Paul','1003') 
+insert into Festival values('2023_3','Datacon','Riya','1004') 
+
+
+
+
+select * from Festival
+
+show database CollegeFestival
+SELECT name FROM sys. databases ;
+SHOW DATABASES;
+use CollegeFestival;
+select * from Festival
+insert into Festival values('2023_4','Codexcite','Riya','1004')
+insert into Festival values('2023_5','Games','Paul','1003')
+
+
+select *from student
+insert into student values('1005','kiya','Delhi','K.K colony','8390540152') 
+insert into student values('1006','Ranveer','Amravati','Philipin Area','9545063187') 
+insert into student values('1007','Sohal','Mumbai','Rajapeth Square','8202662347') 
+insert into student values('1008','Priya','Hyedrabad',' Rajkamal Square','7875013528')
+
+
+select * from Festival
+insert into Festival values('2023_6','DJ','ketty','1005')
+insert into Festival values('2023_7','Present','Panchi','1004')
+insert into Festival values('2023_8','Puzzeld','Samar','1005')
+insert into Festival values('2023_9','Battle','Pratap','1006')
+insert into Festival values('2023_10','Question','Ashok','1007')
+
+
+update student set city_name='Hyedrabad'  where city_name='Chennai' ; 
+update student set city_name='Ahemdabad'  where city_name='Delhi' ; 
