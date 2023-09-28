d03eefc7d70d24083ab332667df4c985ceaf188e
unknown <prajumohod@gmail.com>
2023-09-29 01:20:16 +0530

    Insert_Query_On_Festival_Table

---
+++ SQL/EngiFestival_Database/Insert_Query_On_Festival_Table.sql
@@ -0,0 +1,20 @@
+use EngiFestival;
+select * from Festival
+insert into Festival values('2023_4','Codexcite','Riya','1004')
+insert into Festival values('2023_5','Games','Paul','1003')
+
+
+select *from student
+insert into student values('1005','kiya','Delhi','K.K colony','8390540152') 
+insert into student values('1006','Ranveer','Amravati','Philipin Area','9545063187') 
+insert into student values('1007','Sohal','Mumbai','Rajapeth Square','8202662347') 
+insert into student values('1008','Priya','Hyedrabad',' Rajkamal Square','7875013528')
+
+
+select * from Festival
+insert into Festival values('2023_6','DJ','ketty','1005')
+insert into Festival values('2023_7','Present','Panchi','1004')
+insert into Festival values('2023_8','Puzzeld','Samar','1005')
+insert into Festival values('2023_9','Battle','Pratap','1006')
+insert into Festival values('2023_10','Question','Ashok','1007')
+select * from Festival
