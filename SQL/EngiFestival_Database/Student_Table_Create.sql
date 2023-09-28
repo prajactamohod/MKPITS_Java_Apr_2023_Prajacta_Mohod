0b72a9fd5641bb04801661cdd188901bf9b8acbf
unknown <prajumohod@gmail.com>
2023-09-29 01:22:07 +0530

    Student_Table_Create

---
+++ SQL/EngiFestival_Database/Student_Table_Create.sql
@@ -0,0 +1,10 @@
+create database EngiFestival
+use EngiFestival;
+create table Student
+(
+stud_id char(6) primary key,
+stud_name char(6),
+city_name char(15),
+address varchar(25),
+mobileno varchar(10)
+)
