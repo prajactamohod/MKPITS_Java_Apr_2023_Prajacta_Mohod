f0268d49c96bc044f1ceb55c109042a54d83d94f
unknown <prajumohod@gmail.com>
2023-09-30 22:28:11 +0530

    Pratice_Query_9

---
+++ SQL/Practice_Query/Practice_Query_9.sql
@@ -0,0 +1,13 @@
+--(9) Display the first name, last name, and email of customers who have rented the film titled "Flight Lies".
+ use sakila 
+ show tables
+ 
+ select* from customer
+ select * from rental     
+ select*from inventory 
+ select*from film  
+ 
+ select first_name,last_name,email from customer join rental on
+ rental.customer_id=customer.customer_id join inventory on 
+ inventory.inventory_id=rental.inventory_id join film on
+ film.film_id=inventory.film_id where title="Flight Lies"
