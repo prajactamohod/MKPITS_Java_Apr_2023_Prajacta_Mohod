0426d1a94095d8205fa79d3ce549b31d06e9a27a
unknown <prajumohod@gmail.com>
2023-10-04 22:20:37 +0530

    Practice_Query_47

---
+++ SQL/Practice_Query/Practice_Query_47.sql
@@ -0,0 +1,13 @@
+ -- (47) Get the first name, last name, and email of customers who have rented
+ -- more than 12 films.
+ use sakila
+ show tables
+    select* from customer
+	select* from rental 
+	select*from inventory 
+    select* from film
+    
+ select rental.customer_id, first_name,last_name, count(rental_id) as 'No. of Movies' 
+  from customer join rental on 
+  rental.customer_id=customer.customer_id group by customer_id
+  having count(rental_id)>12
