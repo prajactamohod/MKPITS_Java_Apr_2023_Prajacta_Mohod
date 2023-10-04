d32fb12dc7bb425db5238e80d279169464cfbba7
unknown <prajumohod@gmail.com>
2023-10-04 22:11:10 +0530

    Practice_Query_42

---
+++ SQL/Practice_Query/Practice_Query_42.sql
@@ -0,0 +1,14 @@
+ -- (42) Display the first name, last name, and email of customers who have rented the film titled "Innocent Usual".
+  use sakila
+  show tables
+  select* from customer
+	select* from rental 
+	select*from inventory 
+    select* from film
+    
+ select first_name,last_name,email from customer join rental 
+ on rental.customer_id= customer.customer_id join inventory 
+ on  inventory.inventory_id=rental.inventory_id join film
+ on film.film_id=inventory.film_id where title="Innocent Usual"
+ 
+  
