8f3f93ca70bef3479ee2caaf5fee38b1b9f1de36
unknown <prajumohod@gmail.com>
2023-10-04 22:12:31 +0530

    Practice_Query_44

---
+++ SQL/Practice_Query/Practice_Query_44.sql
@@ -0,0 +1,12 @@
+ -- (44) Retrieve the film titles rented by the customer with customer_id 1100.
+ use sakila
+ show tables
+    select* from customer
+	select* from rental 
+	select*from inventory 
+    select* from film
+    
+ select title from film join inventory on
+ inventory.film_id=film.film_id join rental on
+rental.inventory_id=inventory.inventory_id join customer
+on customer.customer_id=rental.customer_id where customer.customer_id=1100
