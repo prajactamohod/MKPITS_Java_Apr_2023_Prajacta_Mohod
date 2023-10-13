346381b33386c2d810f6a07ce3edc449b7186525
unknown <prajumohod@gmail.com>
2023-10-13 22:54:51 +0530

    Collection_TreeSet_Practice_1

---
+++ Java/Collection_TreeSet_Practice_Programs_Project/Collection_TreeSet_Practice_1.java
@@ -0,0 +1,15 @@
+package collection_treeset_practice_programs;
+
+import java.util.TreeSet;
+public class Collection_TreeSet_Practice_1 {
+    public static void main(String[] args) {
+        TreeSet<String> tree_set = new TreeSet<String>();
+        tree_set.add("Pracjacta");
+        tree_set.add("Alice");
+        tree_set.add("Anaa");
+        tree_set.add("Priya");
+        tree_set.add("Rutuja");
+        System.out.println("Tree set: ");
+        System.out.println(tree_set);
+    }
+}
