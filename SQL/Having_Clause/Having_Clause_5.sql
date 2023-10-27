dd4d62a67edfc57a4ff5288cc3254c908dba1665
unknown <prajumohod@gmail.com>
2023-10-27 22:55:03 +0530

    Having_Clause_5

---
+++ SQL/Having_Clause/Having_Clause_5.sql
@@ -0,0 +1,11 @@
+-- 5. Actors in popular films:
+-- Show actors who have appeared in more than 10 films.
+use sakila
+show tables
+select * from actor
+select * from film_actor
+select * from film
+
+select actor.actor_id, count(actor_id) as "actors_in_film" from actor join film_actor on
+actor.actor_id = film_actor.actor_id join film on
+film_actor.film_id = film.film_id group by actor_id having actors_in_film>10
