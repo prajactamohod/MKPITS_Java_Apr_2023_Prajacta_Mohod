d536f335d9cd11f94c0401135daf62674a130837
unknown <prajumohod@gmail.com>
2023-11-01 22:06:08 +0530

    ArrayList_Pratice_Program_1

---
+++ Java/Practice_Program_Of_ArrayList/arraylist_practice_program/ArrayList_Practice_Program_1.java
@@ -0,0 +1,19 @@
+package arraylist_practice_program;
+
+import java.util.*;
+public class ArrayList_Practice_Program_1 {
+    public static void main(String[] args) {
+
+        List<String> list_Strings = new ArrayList<String>();
+
+        list_Strings.add("Kiya");
+        list_Strings.add("Prajacta");
+        list_Strings.add("Prince");
+        list_Strings.add("Namrata");
+        list_Strings.add("Prachi");
+        System.out.println(list_Strings);
+    }
+}
+
+
+//to create an array list.
