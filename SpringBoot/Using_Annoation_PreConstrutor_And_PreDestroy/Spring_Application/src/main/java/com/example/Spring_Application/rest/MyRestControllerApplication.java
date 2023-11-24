dd2cb00dc7f382d58f240586d63a92b0fb6aa7c2
unknown <prajumohod@gmail.com>
2023-11-24 13:12:40 +0530

    Using_Annotation_PreConstrutor_And_PreDestroy

---
+++ SpringBoot/Using_Annoation_PreConstrutor_And_PreDestroy/Spring_Application/src/main/java/com/example/Spring_Application/rest/MyRestControllerApplication.java
@@ -0,0 +1,32 @@
+package com.example.Spring_Application.rest;
+
+import com.example.Spring_Application.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class MyRestControllerApplication {
+
+    private Account account;         // object created
+
+
+    @Autowired
+
+    public MyRestControllerApplication( @Qualifier("currentAccount") Account account){
+        this.account=account;
+
+
+    }
+
+    @GetMapping("/")
+    public String displayDetails(){
+
+        return account.display();
+    }
+
+
+
+}
