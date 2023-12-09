f26f1cf24f1c422bb9804e423759d7215b7e8f73
unknown <prajumohod@gmail.com>
2023-12-09 18:38:44 +0530

    Collection_TreeMap_Practice_7

---
+++ Java/Practice_Programs/TreeMap/Collection_TreeMap_Practice_7.java
@@ -0,0 +1,17 @@
+package collection_treemap_practice_program;
+
+
+//import java.util.*;
+//import java.util.Map.Entry;
+//public class Collection_TreeMap_Practice_7 {
+//    public static void main(String args[]) {
+//        TreeMap<String, String> tree_map_first = new TreeMap<String, String>(new sort_key());
+//        // Put elements to the map
+//        tree_map_first.put("C2", "Prajacta Mohod");
+//        tree_map_first.put("C4", "Shalini Roy");
+//        tree_map_first.put("C3", "Diya Kapadiya");
+//        tree_map_first.put("C1", "Zaid Kasai");
+//        System.out.println(tree_map_first);
+//    }
+//}
+
