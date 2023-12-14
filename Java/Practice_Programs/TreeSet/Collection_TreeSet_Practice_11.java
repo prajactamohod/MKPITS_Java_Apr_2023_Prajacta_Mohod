4e189b79bb6e19c89412c4d7e08c6dc7787fded8
unknown <prajumohod@gmail.com>
2023-12-14 10:23:36 +0530

    Collection_TreeSet_Practice_11

---
+++ Java/Practice_Programs/TreeSet/Collection_TreeSet_Practice_11.java
@@ -0,0 +1,27 @@
+package collection_treeset_practice_programs;
+
+import java.util.TreeSet;
+import java.util.Iterator;
+public class Collection_TreeSet_Practice_11{
+    public static void main(String[] args) {
+        // creating TreeSet
+        TreeSet <Integer>tree_number = new TreeSet<Integer>();
+        TreeSet <Integer>treeheadset = new TreeSet<Integer>();
+
+        // Add numbers in the tree
+        tree_number.add(10);
+        tree_number.add(22);
+        tree_number.add(36);
+        tree_number.add(25);
+        tree_number.add(16);
+        tree_number.add(70);
+        tree_number.add(82);
+        tree_number.add(89);
+        tree_number.add(14);
+
+        System.out.println("Less than or equal to 86 : "+tree_number.floor(86));
+        System.out.println("Less than or equal to 29 : "+tree_number.floor(29));
+    }
+}
+
+// code of  get the element in a tree set less than or equal to the given element
